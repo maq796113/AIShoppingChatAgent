38f16148e9fc2e34ca792be8a490623d8b4d7153
Shubbair <hussainsalih.github@gmail.com>
2024-01-04 01:28:09 +0300

    update code with streamlit

--- demo.py
+++ demo.py
@@ -40,35 +40,68 @@ if not google_key:
         "Please follow the instructions in the README to set it up.")
 
 genai.configure(api_key=google_key)
-generation_config = genai.types.GenerationConfig(
-        temperature=0.4,
-        max_output_tokens=1024,
-        top_k=4,
-        top_p=1)
-
-model = genai.GenerativeModel('gemini-pro')
+generation_config = {
+  "temperature": 0,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+ 
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  }
+]
+model = genai.GenerativeModel(model_name="gemini-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
 st.title("AI Search Agent")
 text_search = st.text_input("Search videos by title or speaker", value="")
 
+response : str = ''
+
+col1, col2 = st.columns(2)
+
+def click_button():
+    clicked = True
+
 if text_search:
-    get_data(text_search)
+    # get_data(text_search)
     
     with open('results.json') as json_file:
         json_data = json.load(json_file)
     
-    if(json_data):
-        prompt = f"Give me top 4 from this json : {json_data['shopping_results']}"
-        print(prompt)
-        response = model.generate_content(
-            prompt,
-            stream=True,
-            generation_config=generation_config)
-        
-        chatbot = ""
-        for chunk in response:
-            for i in range(0, len(chunk.text), 10):
-                section = chunk.text[i:i + 10]
-                chatbot += section
-                # time.sleep(0.01)
-        st.text(chatbot)
+    # if(json_data):
+    #     prompt = f"Give me title,link,price and thumbnail from {json_data['shopping_results']}"
+    #     response = model.generate_content(
+    #         prompt,
+    #         stream=True,
+    #         generation_config=generation_config)
+
+    # chatbot = []
+    # for chunk in response:
+    #     for i in range(0,len(chunk.text),10):
+    #         chatbot.append(chunk.text)
+    # if clicked == True:
+    for item in json_data['shopping_results'][:5]:
+        with st.container(): 
+          st.text(item['title'])
+          st.image(item['thumbnail'],width=100)
+          st.link_button('Product Market : ' + item['source'],item['link'])
+          st.text('Price : ' + item['price'])
+          st.divider()
+    st.button('load more...',on_click=click_button)
