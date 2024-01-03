a71af749f40ebc0d81801de14935380306adc0c5
Shubbair <hussainsalih.github@gmail.com>
2024-01-03 11:27:10 +0300

    update code

--- demo.py
+++ demo.py
@@ -1,11 +1,15 @@
 from serpapi import GoogleSearch
 import os
+import time
+import uuid
 import json
-import gradio as gr
+import streamlit as st
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+GOOGLE_API_KEY = os.environ.get('API')
 SEARCH_API = os.environ.get('SEARCH_API')
 
 def get_data(keyword: str):
@@ -19,11 +23,20 @@ def get_data(keyword: str):
     search = GoogleSearch(params)
     results = search.get_dict()
     json.dump(results, open('results.json', 'w'), indent=2)
-get_data('black hoodie')
 
-# try to make access 
 
 # from google_web_scrappper import get_data
 # get_data(prompt_input)
 # with open('results.json') as json_file:
-#                      json_data = json.load(json_file)
+#     json_data = json.load(json_file)
+
+text_search = st.text_input("Search videos by title or speaker", value="")
+
+if text_search:
+    get_data(text_search)
+    
+    with open('results.json') as json_file:
+        json_data = json.load(json_file)
+    
+    # print(json_data)
+    st.json(json_data)
